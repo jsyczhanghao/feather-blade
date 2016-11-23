dbefb2c509f7aa0aa80a3e777e6c520306af0298
zhanghao <jsyczhanghao@sina.com>
2016-11-23 12:03:02 +0800

    fixed blade 5.1以下，调用compileString 导致死循环的问题

--- src/Compiler.php
+++ src/Compiler.php
@@ -18,6 +18,54 @@ class Compiler extends BladeCompiler{
     protected $customDirectives = [];
 
     /**
+     * Compile the view at the given path.
+     *
+     * @param  string  $path
+     * @return void
+     */
+    public function compile($path = null)
+    {
+        if ($path) {
+            $this->setPath($path);
+        }
+
+        $contents = $this->compileString($this->files->get($this->getPath()));
+
+        if (!is_null($this->cachePath)) {
+            $this->files->put($this->getCompiledPath($this->getPath()), $contents);
+        }
+    }
+
+    /**
+     * Compile the given Blade template contents.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function compileString($value)
+    {
+        $result = '';
+        $this->footer = array();
+
+        // Here we will loop through all of the tokens returned by the Zend lexer and
+        // parse each one into the corresponding valid PHP. We will then have this
+        // template as the correctly rendered PHP that can be rendered natively.
+        foreach (token_get_all($value) as $token) {
+            $result .= is_array($token) ? $this->parseToken($token) : $token;
+        }
+
+        // If there are any footer lines that need to get added to a template we will
+        // add them here at the end of the template. This gets used mainly for the
+        // template inheritance via the extends keyword that should be appended.
+        if (count($this->footer) > 0) {
+            $result = ltrim($result, PHP_EOL)
+                    .PHP_EOL.implode(PHP_EOL, array_reverse($this->footer));
+        }
+
+        return $result;
+    }
+
+    /**
      * Register a handler for custom directives.
      *
      * @param  string  $name
